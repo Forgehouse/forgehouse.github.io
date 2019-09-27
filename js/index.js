0122bf9737f58b052e20a6aa90eaf04dd71f2c01
Cai Lehwald <cailehwald@googlemail.com>
2019-09-27 11:51:46 +0100

    Responsiveness, smooth scrolling and panel templating
    Try out some repeatable textures
    Add css to make panels responsive
    Add javascript and jquery to smoothly scroll to a section on navigation.
    add the brand logo

--- js/index.js
+++ js/index.js
@@ -1,4 +1,10 @@
 function setCopyRightDate() {
     $('#copyrightDate').text(new Date().getFullYear());
 }
+
+function scrollToId(id) {
+    $('html, body').animate({
+        scrollTop: $("#" + id).offset().top - 50
+    }, 2000);
+}
 var wow = new WOW();
